d85f625c1b47b4cb67ab8c3f83e8b5c4178fb3a8
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2022-12-05 15:09:04 +0100

    jkhkj

---
+++ js/main.js
@@ -0,0 +1,104 @@
+const { log } = console;
+
+//if
+// const user = await User.find();
+// let age = 15;
+// if (age <= 15) {
+//   console.log("you are too young to vote");
+// } else if (age > 15 && age <= 60) {
+//   console.log("you are to eligible to vote");
+// } else {
+//   console.log("you are too old to vote");
+// }
+
+const name = "john";
+const age = "";
+const country = "togo";
+
+let list = [
+  { name: "john", age: 15, country: "Nigeria" },
+  { name: "john", age: 60, country: "Nigeria" },
+  { name: "jane", age: 60, country: "Nigeria" },
+  { name: "jacob", age: 30, country: "Togo" },
+  { name: "james", age: 20, country: "England" },
+  { name: "john", age: 60, country: "Togo" },
+  { name: "john", age: 45, country: "Togo" },
+];
+
+if (name) {
+  list = list.filter((item) => item.name.toLowerCase() === name.toLowerCase());
+}
+
+if (age) {
+  list = list.filter((item) => item.age == age);
+}
+
+if (country) {
+  list = list.filter(
+    (item) => item.country.toLowerCase() === country.toLowerCase()
+  );
+}
+
+console.log(list);
+
+// const h2 = document.getElementById("header"); //get element by id
+// const btn = document.querySelectorAll(".btn");
+// log(btn);
+// log(h2);
+// h2.style.backgroundColor = "blue";
+// btn[0].style.padding = "10px 20px";
+// btn[0].style.borderRadius = "20px";
+
+// for (let i = 0; i < btn.length; i++) {
+//   log(btn[i]);
+//   btn[i].addEventListener(
+//     "click",
+//     () =>
+//       (btn[i].innerHTML = `<div>
+//      <h2>Hello</h2>
+//   </div>`)
+//   );
+// }
+
+// btn.forEach((element) => {
+//   log(element, "eleent");
+// });
+// alert("HEllo 3");
+let a = {};
+console.log(typeof a);
+
+var b = 12;
+
+var b = 20;
+
+let c = 20;
+c = 30;
+
+//    const d = 20
+//         d =10
+
+// const country = ["Nigeria", "Ghana"];
+
+// const result = `The nae of my country is ${country[0]}`;
+
+// const quote = "He said and  i quote, 'I amm the best' ";
+
+// log(result);
+
+// const list = "Nigeria, Niger, Ghana, Togo, Germany";
+
+// const arr = list.split();
+// log(arr);
+
+const g = 20;
+const h = "20";
+
+log(g == h);
+
+function ab(x) {
+  if (NaN) {
+    console.log("true");
+  }
+}
+
+ab();
