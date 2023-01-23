0cfae4bbb71c741f27bd3886e81a657e87120abc
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2023-01-23 13:53:34 +0100

    DOM manipulation

--- js/DOM.js
+++ js/DOM.js
@@ -78,3 +78,52 @@ btn.addEventListener("click", (e) => {
 });
 
 // const searchHandler = ;
+
+//querySelector
+//returns the first element
+const h2ByQuerySelector = document.querySelector(".h2-text");
+console.log(h2ByQuerySelector, "h2Query");
+
+//query selector all
+const h2ByQuerySelectorAll = document.querySelectorAll(".h2-text");
+console.log(h2ByQuerySelectorAll, "h2QueryAll");
+
+//adding attribute
+h2ByQuerySelectorAll[0].style.color = "white";
+h2ByQuerySelectorAll[0].src = "";
+h2ByQuerySelectorAll[0].classList.add("title");
+h2ByQuerySelectorAll[0].classList.remove("title");
+h2ByQuerySelectorAll[0].classList.toggle("total");
+
+h2ByQuerySelectorAll[0].addEventListener("mouseover", function () {
+  h2ByQuerySelectorAll[0].innerHTML = `<span>Hello</span>`;
+});
+
+const ul = document.querySelector("ul");
+document.getElementById("show-list").addEventListener("click", function () {
+  const list = `
+<li>items 1</li>
+<li>items 2</li>
+<li>items 3</li>
+`;
+  ul.innerHTML = list;
+});
+
+console.log(document.getElementById("add-item"));
+
+document.getElementById("add-item").addEventListener("click", function () {
+  console.log("here");
+  const item = document.createElement("div");
+  item.className = "item mr-3 bg-slate-400";
+
+  const camera = "lorem 1";
+  const desc = "lorem ipsum lorme";
+  const itemContent = `<h4>${camera}</h4>
+  <p>${desc}</p>`;
+
+  item.innerHTML = itemContent;
+
+  const items = document.getElementsByClassName("items")[0];
+
+  items.appendChild(item);
+});
