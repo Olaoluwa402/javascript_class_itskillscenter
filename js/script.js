e16c5c5bcb0d4b8b0d019e273b2c0cc7510bb52e
olaoluwa402 <ibukundaniel402@yahoo.co.uk>
2023-01-24 15:52:05 +0100

    functional programming

--- js/script.js
+++ js/script.js
@@ -15,6 +15,31 @@ function render() {
     //add event listeer to each btn
     btnElement.addEventListener("click", addToCartClicked);
   }
+
+  // listen for input changed
+  const inputChangedListener =
+    document.getElementsByClassName("cart-items-qty");
+  for (let i = 0; i < inputChangedListener.length; i++) {
+    const inputElement = inputChangedListener[i];
+    //add event listeer to each btn
+    inputElement.addEventListener("change", inputChanged);
+  }
+
+  const removeCartItemBtn =
+    document.getElementsByClassName("cart-items-remove");
+  for (let i = 0; i < removeCartItemBtn.length; i++) {
+    const cartItemElement = removeCartItemBtn[i];
+    //add event listeer to each btn
+    cartItemElement.addEventListener("click", removeCartItem);
+  }
+
+  const purchaseItemsBtn =
+    document.getElementsByClassName("cart-item-purshase");
+  for (let i = 0; i < purchaseItemsBtn.length; i++) {
+    const purchaseElement = purchaseItemsBtn[i];
+    //add event listeer to each btn
+    purchaseElement.addEventListener("click", purchaseItems);
+  }
 }
 
 //addToCartClicked
@@ -85,4 +110,70 @@ function addToCart(title, price, imgSrc) {
 
   //append the tr to the tbody
   cart.appendChild(tr);
+
+  //   update cate total
+  updateCartTotal();
+}
+
+//update cart total
+function updateCartTotal() {
+  const cart = document.getElementsByTagName("tbody")[0];
+  const cartItems = cart.getElementsByClassName("cart-items");
+
+  //   initiate a start sum
+  let total = 0;
+  for (let item of cartItems) {
+    console.log(item);
+    const priceElement = item.getElementsByClassName("cart-items-price")[0];
+    const price = parseFloat(priceElement.textContent.replace("₦", ""));
+    const qtyElement = item.getElementsByClassName("cart-items-qty")[0];
+    const qty = parseInt(qtyElement.value);
+
+    total = total + price * qty;
+  }
+
+  //   round to 2 decimal place //Math.round() and toFixed()
+  const totalPrice = Math.round(total * 100) / 100;
+
+  //add total to the DOM
+  document.getElementsByClassName(
+    "cart-total"
+  )[0].textContent = `Total: ${totalPrice}`;
+
+  console.log(totalPrice, "total");
+}
+
+function inputChanged(e) {
+  const input = e.target;
+
+  console.log(isNaN(input.value), "isNaN(input.value)");
+
+  if (input.value < 1) {
+    input.value = 1;
+  }
+  updateCartTotal();
+}
+
+function removeCartItem(e) {
+  const removeBtn = e.target;
+  const decision = confirm("Are you sure about reovng this item?");
+  console.log(decision, "decision");
+  if (!decision) {
+    return;
+  }
+
+  removeBtn.parentElement.parentElement.remove();
+
+  updateCartTotal();
+}
+
+function purchaseItems() {
+  //   console.log(document.getElementsByTagName("tbody")[0].hasChildNodes());
+  if (!document.getElementsByTagName("tbody")[0].hasChildNodes) {
+    alert("Your cart is empty");
+    return;
+  }
+
+  document.getElementsByTagName("tbody")[0].innerHTML = "";
+  updateCartTotal();
 }
